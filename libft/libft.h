4701dd77644ace5e1eeeb52aa7524375e89eb1e7
Diogo Delgado <ddelgado@e1z2r5p35.42.us.org>
2016-10-11 14:44:50 -0700

    ft_memcmp 1.0

--- libft/libft.h
+++ libft/libft.h
@@ -43,5 +43,6 @@ char		*ft_strnstr(const char *big, const char *little, size_t len);
 void		*ft_memcpy_back(void *restrict dest, 
 	const void *restrict src, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
