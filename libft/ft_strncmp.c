ad05afcc4628c5a995a02c3d676e9c3e602c5a07
Diogo Delgado <ddelgado@e1z2r5p35.42.us.org>
2016-10-11 15:30:24 -0700

    ft_memcmp 1.11

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *source_1, const char *source_2, size_t n)
 	unsigned char	*s1 = (unsigned char *) source_1;
 	unsigned char	*s2 = (unsigned char *) source_2;
 
-	while ((*s1 || *s2) && n > 0)
+	while ((*s1 && *s2) && n > 0)
 	{
 		if (*s1 != *s2)
 		{
