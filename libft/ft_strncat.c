f7123d22501567688ed17841f22c11022449884a
thedmpd <dmpd88@gmail.com>
2016-10-18 11:09:10 -0700

    ft_strncat.c

--- libft/ft_strncat.c
+++ libft/ft_strncat.c
@@ -1,22 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddelgado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/25 11:52:21 by ddelgado          #+#    #+#             */
-/*   Updated: 2016/09/25 11:52:22 by ddelgado         ###   ########.fr       */
+/*   Created: 2016/10/18 11:06:01 by ddelgado          #+#    #+#             */
+/*   Updated: 2016/10/18 11:06:02 by ddelgado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-/*
-The strcat() function append a copy of the null-terminated string s2 to the 
-end of the null-terminated string s1, then add a terminating '\0'. The string 
-s1 must have sufficient space to hold the result.
-*/
-char	*ft_strcat(char s1, const char s2)
-{
-	return (ft_strcpy(s1 + ft_strlen(s1), s2));
-}
+
--- libft/libft.h
+++ libft/libft.h
@@ -50,5 +50,6 @@ int			ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t		ft_strclen(const char *str, char c);
 void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
 char		*ft_strncpy(char *dest, const char *src, size_t n);
+char		*ft_strncat(char *dest, const char *src, size_t n);
 
 #endif
