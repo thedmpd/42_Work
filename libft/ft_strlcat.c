c0fab21e26a931d57001f617d49dead8ae987582
thedmpd <dmpd88@gmail.com>
2016-10-18 15:00:49 -0700

    ft_strdup

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -14,13 +14,15 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	unsigned	int i;
-	unsigned	int j;
-	unsigned	int k;
+	size_t i;
+	size_t j;
+	size_t k;
 
 	i = 0;
 	while (dest[i] != '\0')
 		i++;
+	if((size - i) == 0)
+		return(i - ft_strlen(dest) + ft_strlen(src));
 	j = 0;
 	while (src[j] != '\0' && j < size)
 	{
