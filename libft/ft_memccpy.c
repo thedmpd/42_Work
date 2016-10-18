a93793c7954f372607e717368f8e5438a8ff6b91
thedmpd <dmpd88@gmail.com>
2016-10-18 12:06:45 -0700

    update memccpy

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -15,20 +15,12 @@
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	size_t i;
-
-	i = ft_strclen(src,c);
-	if(ft_memchr(src,c,n) == NULL)
-		return(NULL);
-	else if(i < n - 1)
+	while(n != 0)
 	{
-		//printf("%zu\n", ft_strclen(src,c));
-		dest = ft_strncpy(dest,src, i);
-		return(dest + i);
+		*(unsigned char *)dest = *(unsigned char *)src;
+		if(*(unsigned char *)src == (unsigned char)c)
+			return(dest + 1);
+		n--;
 	}
-	else
-	{
-		dest = ft_strncpy(dest,src,n);
-		return(dest + n);
-	}
+	return(NULL);
 }
