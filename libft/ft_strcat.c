7290d5fd61f3d59efd93bf8bc1d5657be8ec1dc1
thedmpd <dmpd88@gmail.com>
2016-10-18 10:51:46 -0700

    ft_memccpy.c

--- libft/ft_strcat.c
+++ libft/ft_strcat.c
@@ -16,7 +16,7 @@ The strcat() function append a copy of the null-terminated string s2 to the
 end of the null-terminated string s1, then add a terminating '\0'. The string 
 s1 must have sufficient space to hold the result.
 */
-char	*ft_strcat(char s1, const char s2)
+char	*ft_strcat(char *restrict s1, const char *restrict s2)
 {
 	ft_strcpy(s1 + ft_strlen(s1), s2);
 	return (s1);
