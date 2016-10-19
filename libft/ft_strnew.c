3059bd95d94b1b58e7095b90f919ae2e4a88a065
thedmpd <dmpd88@gmail.com>
2016-10-18 22:06:17 -0700

    strlcat.c

--- libft/ft_strnew.c
+++ libft/ft_strnew.c
@@ -1,25 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_malloc.c                                        :+:      :+:    :+:   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddelgado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/10/18 13:41:42 by ddelgado          #+#    #+#             */
-/*   Updated: 2016/10/18 13:41:44 by ddelgado         ###   ########.fr       */
+/*   Created: 2016/10/18 22:03:19 by ddelgado          #+#    #+#             */
+/*   Updated: 2016/10/18 22:03:21 by ddelgado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdlib.h>
-
-void	*ft_memalloc(size_t size)
+char	*ft_strnew(size_t size)
 {
-	void *ptr;
+	char *new_string;
 
-	ptr = malloc(size);
-	if(!ptr)
-		return(NULL);
-	ft_memset(ptr, '\0', size);
-	return(ptr);
+	*new_string = ft_memalloc(size);
+	return(new_string);
 }
