343d73a6c15f4dd9c166054ae93fce18fff61812
thedmpd <dmpd88@gmail.com>
2016-10-18 21:00:54 -0700

    strlcat.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *source_1, const char *source_2, size_t n)
 
 	s1 = (unsigned char *) source_1;
 	s2 = (unsigned char *) source_2;
-	while (n > 0)
+	while (n > 0 && *s1 && *s2)
 	{
 		if (*s1 != *s2)
 		{
