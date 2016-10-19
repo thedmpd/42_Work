b223510aa7e041fbce1b29c825158967188bd140
thedmpd <dmpd88@gmail.com>
2016-10-18 22:00:14 -0700

    strlcat.c

--- libft/ft_memdel.c
+++ libft/ft_memdel.c
@@ -1,25 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_malloc.c                                        :+:      :+:    :+:   */
+/*   ft_memdel.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddelgado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/10/18 13:41:42 by ddelgado          #+#    #+#             */
-/*   Updated: 2016/10/18 13:41:44 by ddelgado         ###   ########.fr       */
+/*   Created: 2016/10/18 21:56:54 by ddelgado          #+#    #+#             */
+/*   Updated: 2016/10/18 21:56:55 by ddelgado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-void	*ft_memalloc(size_t size)
+void	ft_memdel(void **ap)
 {
-	void *ptr;
-
-	ptr = malloc(size);
-	if(!ptr)
-		return(NULL);
-	ft_memset(ptr, 0, size);
-	return(ptr);
+	free(*ap);
+	*ap = NULL;
 }
