32b8534490b6cb3cf9a45e5d77d97a5614e90ab2
thedmpd <dmpd88@gmail.com>
2016-10-18 21:13:20 -0700

    strlcat.c

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -14,5 +14,20 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	return (ft_strncmp(s1, s2, n));
+	unsigned char	*s1;
+	unsigned char	*s2;
+
+	s1 = (unsigned char *) source_1;
+	s2 = (unsigned char *) source_2;
+	while (n > 0)
+	{
+		if ((*s1 != *s2))
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+		n--;
+	}
+	return (0);;
 }
