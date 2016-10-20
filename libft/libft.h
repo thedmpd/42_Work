8affd1aef429a7e691b291a42f0fc67b6fea0c90
thedmpd <dmpd88@gmail.com>
2016-10-20 13:56:09 -0700

    ft_strrev.c

--- libft/libft.h
+++ libft/libft.h
@@ -63,5 +63,6 @@ void		ft_striter(char *s, void (*f)(char *));
 char		*ft_strmap(char const *s, char (*f)(char));
 void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 void		ft_putendl(char const *s);
+char		*ft_strrev(char *s);
 
 #endif
