920dab6742b258f46511c375dd1563780a23d48f
thedmpd <dmpd88@gmail.com>
2016-10-20 15:34:25 -0700

    ft_strequ.c

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *new_string;
+
+	new_string = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (!new_string)
+		return (NULL);
+	new_string = ft_strcpy(new_string, s1);
+	new_string = ft_strcat(new_string, s2);
+	return (new_string);
+}
