043ba85aca2c34813916c1d53aadf13d2c6bd239
thedmpd <dmpd88@gmail.com>
2016-10-20 15:47:28 -0700

    ft_strequ.c

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddelgado <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/20 15:38:57 by ddelgado          #+#    #+#             */
+/*   Updated: 2016/10/20 15:38:58 by ddelgado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	char	*sub;
+	int		i;
+
+	while (ft_whitespace(*s))
+		s++;
+	i = 0;
+	while (s[i])
+		i++;
+	while (ft_whitespace(s[i]) && i > 0)
+		i--;
+	sub = ft_strsub(s, 0 , i + 1);
+	if (sub)
+		return (sub);
+	else
+		return (NULL);
+}
