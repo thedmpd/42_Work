afa92cb4a9793e3a280473ebb7ccb4725dfe766a
thedmpd <dmpd88@gmail.com>
2016-10-20 11:37:18 -0700

    ft_striteri

--- libft/libft.h
+++ libft/libft.h
@@ -61,5 +61,6 @@ void		ft_strdel(char **as);
 void		ft_strclr(char *s);
 void		ft_striter(char *s, void (*f)(char *));
 char		*ft_strmap(char const *s, char (*f)(char));
+void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 #endif
