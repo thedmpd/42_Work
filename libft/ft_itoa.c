b7db5c8131590774e4e2e533d10f42aec5348c30
thedmpd <dmpd88@gmail.com>
2016-10-20 14:16:27 -0700

    ft_strrev.c

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -10,4 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char *ft_itoa(int n)
+{
+	char	*new_string;
+	int		i;
+	int		neg;
+	int		remainder;
+	long	long_n;
+
+	long_n = (long)n;
+	new_string = ft_strnew(ft_digitlen(n));
+	i = 0;
+	neg = 0;
+	new_string[i] = '\0';
+	if (long_n == 0)
+		return (new_string);
+	neg = (long_n < 0) ? 1 : 0;
+	long_n = (long_n < 0) ? -long_n : long_n;
+	while (long_n != 0)
+	{
+		remainder = long_n % 10;
+		new_string[i] = remainder + '0';
+		long_n = long_n / 10;
+		i++;
+	}
+	new_string[i] = neg ? '-' : new_string[i];
+	return (ft_strrev(new_string));
+}
