7f2fe531878aa21ed9365a7ff12e896f46f38998
thedmpd <dmpd88@gmail.com>
2016-10-20 20:54:03 -0700

    hello

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -19,9 +19,9 @@ char	**ft_strsplit(const char *s, char c)
 	size_t	j;
 	size_t	len;
 
-	if (!s || !c)
-		return (0);
 	ret = ft_memalloc(ft_strlen(s) + 1);
+	if (!s || !c || !ret)
+		return (NULL);
 	i = 0;
 	j = 0;
 	while (s[i])
