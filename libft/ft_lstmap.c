24122ec55420f3c733c0d03378f7121d24ad4c1a
thedmpd <dmpd88@gmail.com>
2016-10-20 23:24:18 -0700

    hi

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-t_list      *ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
     t_list      *holder;
  
--- libft/libft.h
+++ libft/libft.h
@@ -93,6 +93,6 @@ void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list			*ft_lstnew(void const *content, size_t content_size);
-t_list			*ft_lstnew(void const *content, size_t content_size);
+t_list      	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 #endif
