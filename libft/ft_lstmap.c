b18324419c86c31614307345ff068bbb1444cb9d
thedmpd <dmpd88@gmail.com>
2016-10-20 22:57:36 -0700

    lists

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -18,7 +18,7 @@ t_list  *ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
  
         if (lst && f)
         {
-                return reccur(f(lst),lst->next,f)
+                return reccur(f(lst),lst->next,f);
         }
         return (0);
 }
