a5f9029bc58257ff1b36c8c84b5ad72ba64ea115
thedmpd <dmpd88@gmail.com>
2016-10-20 22:53:02 -0700

    lists

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -14,12 +14,26 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list	*sortie;
-	if (lst)
+	t_list	*nlst;
+	t_list	*tmp;
+	t_list	*tmp2;
+	if (!lst || !f)
+		return (NULL);
+	tmp2 = f(lst);
+	nlst = ft_lstnew(tmp2->content, tmp2->content_size);
+	if (nlst)
 	{
-		sortie = f(lst);
-		sortie->next = ft_lstmap(lst->next, f);
-		return (sortie);
+		tmp = nlst;
+		lst = lst->next;
+		while (lst)
+		{
+			tmp2 = f(lst);
+			tmp->next = ft_lstnew(tmp2->content, tmp2->content_size);
+			if (!(tmp->next))
+				return (NULL);
+			tmp = tmp->next;
+			lst = lst->next;
+		}
 	}
-	return (NULL);
+	return (nlst);
 }
