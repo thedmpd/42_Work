88a06f51cc08c5d3da993ac8e09ca571c1c5822f
thedmpd <dmpd88@gmail.com>
2016-10-20 22:24:32 -0700

    lists

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -16,11 +16,7 @@ void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
 	if (alst != NULL)
 	{
-		if (*alst != NULL)
-		{
-			(*del)((**alst).content, (**alst).content_size);
-			ft_memdel((void)*alst);
-		}
-		*alst = NULL;
+		(*del)((**alst).content, (**alst).content_size);
+		ft_memdel((void)*alst);
 	}
 }
