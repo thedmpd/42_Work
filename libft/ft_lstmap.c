cbd87d26317d1478d1ee3cbe97f890c05bd3ec32
thedmpd <dmpd88@gmail.com>
2016-10-20 22:59:57 -0700

    lists

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -12,24 +12,27 @@
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list*))
 {
-	t_list	*new;
-	t_list	*link;
+	t_list	*result;
+	t_list	*tmp;
+	t_list	*tmp2;
 
-	new = NULL;
-	link = NULL;
-	while (lst != NULL)
+	if (!lst || !f)
+		return (NULL);
+	tmp2 = f(lst);
+	if ((result = ft_lstnew(tmp2->content, tmp2->content_size)))
 	{
-		if (new == NULL)
+		tmp = result;
+		lst = lst->next;
+		while (lst)
 		{
-			new = f(lst);
-			link = new;
-		}
-		else
-		{
-			link->next = f(lst);
+			tmp2 = f(lst);
+			if (!(tmp->next = ft_lstnew(tmp2->content, tmp2->content_size)))
+				return (NULL);
+			tmp = tmp->next;
+			lst = lst->next;
 		}
 	}
-	return (new);
+	return (result);
 }
